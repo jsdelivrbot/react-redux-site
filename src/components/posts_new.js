7735ba6265d6a208300c711b1f1e5ffd98da311a
nini-faroux <nfarrel1@tcd.ie>
2018-02-08 23:09:46 +0100

    clean up render func

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,10 +2,11 @@ import React, { Component } from "react";
 import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
-  renderTitleField(field) {
+  renderField(field) {
     return (
-      <div>
-        <input type="text" {...field.input} />
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
       </div>
     );
   }
@@ -13,7 +14,13 @@ class PostsNew extends Component {
   render() {
     return (
       <form>
-        <Field name="title" component={this.renderTitleField} />
+        <Field label="Title" name="title" component={this.renderField} />
+        <Field label="Tags" name="tags" component={this.renderField} />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
       </form>
     );
   }
